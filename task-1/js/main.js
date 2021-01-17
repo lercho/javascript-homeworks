d9d8d27e905c1503baa44c8df38f74ad6ddd6de7
Lera <lerchenok@ukr.net>
2021-01-17 12:55:33 +0200

    3

--- task-1/js/main.js
+++ task-1/js/main.js
@@ -79,16 +79,40 @@ function addItem(arr, obj) {
     }
 }
 
-function isBought(arr, obj) {
-
+function Buy(arr, obj) {
+    let isInList = false;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name = obj.name) {
+            isInList = true;
+            if (arr[i].bought == false) {
+                arr[i].bought = true;
+                return true;
+            } else if (arr[i].bought = true) {
+                throw "This product is already bought";
+                return false;
+            }
+        }
+    }
+    if (isInList == false) {
+        throw "This product is not in the list";
+        return false;
+    }
 }
 
 // showList(shoppingList);
 
-// let newItem = {}
-// newItem.name = prompt("Введите название продукта");
-// newItem.quantity = +prompt("Введите количество");
+// let newItem = {};
+// newItem.name = prompt("Enter the product name");
+// newItem.quantity = +prompt("Enter quantity");
 // newItem.bought = false;
 
 // addItem(shoppingList, newItem);
 
+let item = {};
+item.name = prompt("Which product did you buy?");
+
+if (Buy(shoppingList, item)) {
+    alert("Great! Let's go on shopping");
+} else {
+    alert("This product is either already bought or was not in the list");
+}
---
+++ task-3/js/main.js
@@ -0,0 +1,32 @@
+let stylesArr = [
+    {
+        name: "color",
+        value: "blue",
+    },
+    {
+        name: "font-size",
+        value: "16px",
+    },
+    {
+        name: "font-weight",
+        value: "bold",
+    },
+    {
+        name: "text-decoration",
+        value: "underline",
+    },
+    {
+        name: "background-color",
+        value: "yellow",
+    },
+]
+
+let someText = "Мороз и солнце - день чудесный";
+
+function showText(arr, text) {
+    document.write(
+        '<p style = "' + arr[0].name + ': ' + arr[0].value + '">' + text + '</p>'
+    );
+}
+
+showText(stylesArr, someText);
